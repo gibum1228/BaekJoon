8928f6bb4cd83b888f6253a5db10efcf103d2d96
gibum1228 <gibum1228@naver.com>
2019-08-02 00:17:34 +0900

    백준사이트 10171번 문제

---
+++ Java/src/B10171.java
@@ -0,0 +1,20 @@
+/*
+문제
+아래 예제와 같이 고양이를 출력하시오.
+입력
+없음
+출력
+고양이를 출력한다.
+ */
+public class B10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
