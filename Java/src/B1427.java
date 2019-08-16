586745735127a75f6bbf3292833d2ac175ef0580
gibum1228 <gibum1228@naver.com>
2019-08-16 23:45:51 +0900

    소트인사이드

---
+++ Java/src/B1427.java
@@ -0,0 +1,38 @@
+/*
+문제
+배열을 정렬하는 것은 쉽다. 수가 주어지면, 그 수의 각 자리수를 내림차순으로 정렬해보자.
+입력
+첫째 줄에 정렬하고자하는 수 N이 주어진다. N은 1,000,000,000보다 작거나 같은 자연수이다.
+출력
+첫째 줄에 자리수를 내림차순으로 정렬한 수를 출력한다.
+ */
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+
+public class B1427 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String text = sc.nextLine();
+		String[] charList = text.split("");
+		
+		for(int i = 0; i < charList.length; i++) {
+			System.out.println(charList[i]);
+		}
+		
+		List<String> List = Arrays.asList(charList);
+		
+		Collections.reverse(List);
+		
+		for(int i = 0; i < List.size(); i++) {
+			System.out.print(List.get(i));
+		}
+		
+		sc.close();
+	}
+
+}
