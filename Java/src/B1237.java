6b4b8f384aada52f7ac7bea154208c9b2125a912
gibum1228 <gibum1228@naver.com>
2019-10-05 10:37:52 +0900

    우주를 정복

---
+++ Java/src/B1237.java
@@ -0,0 +1,16 @@
+/*
+문제
+이 문제를 푸는 자 우주를 정ㅋ벅ㅋ한다.
+입력
+우주를 정ㅋ벅ㅋ하는 자에겐 입력 따위 필요 없다.
+출력
+첫째 줄에 문제의 정답을 출력한다.
+ */
+public class B1237 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("문제의 정답");
+	}
+
+}
