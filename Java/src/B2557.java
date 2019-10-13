be07b4de43b06a6b05dac1cca8a0d28ec4d69bb8
gibum1228 <gibum1228@naver.com>
2019-10-13 21:11:49 +0900

    Hello World!

---
+++ Java/src/B2557.java
@@ -0,0 +1,16 @@
+/*
+문제
+Hello World!를 출력하시오.
+입력
+없음
+출력
+Hello World!를 출력하시오.
+ */
+public class B2557 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.print("Hello World!");
+	}
+
+}
