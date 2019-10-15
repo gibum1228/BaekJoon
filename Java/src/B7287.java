ff3073c17a44f6d08071cbe9badf15d0b38742e3
gibum1228 <gibum1228@naver.com>
2019-10-16 01:41:21 +0900

    자신의 아이디와 맞은 문제 갯수 출력

---
+++ Java/src/B7287.java
@@ -0,0 +1,17 @@
+/*
+문제
+자신이 온라인 저지에서 맞은 문제의 개수와 아이디를 그대로 출력하는 프로그램을 작성하시오.
+입력
+이 문제는 입력이 없다.
+출력
+첫 줄에 자신이 맞은 문제의 수, 둘째 줄에 아이디를 출력한다.
+ */
+public class B7287 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println(48);
+			System.out.print("gibum1228");
+	}
+
+}
