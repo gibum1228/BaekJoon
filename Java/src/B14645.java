b3515eef1b255a2af3afe2deca8a9b89e87fe837
gibum1228 <gibum1228@naver.com>
2020-03-15 11:23:17 +0900

    와이버스 부릉부릉

---
+++ Java/src/B14645.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class B14645 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int a = sc.nextInt();
+		int b = 0;
+		
+		for(int i = 0; i < n; i++) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+		}
+		
+		System.out.println("ºñ¿ÍÀÌ");
+		
+		sc.close();
+	}
+
+}
