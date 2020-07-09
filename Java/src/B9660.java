c0c18b84a79088f1e54e360cdb824fb5a681d60a
gibum1228 <gibum1228@naver.com>
2020-07-09 11:03:18 +0900

    돌 게임 6

---
+++ Java/src/B9660.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class B9660 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		Long n = sc.nextLong();
+		
+		if(n % 7 == 0 || n % 7 == 2) {
+			System.out.println("CY");
+		}else {
+			System.out.println("SK");
+		}
+		
+		sc.close();
+	}
+
+}
