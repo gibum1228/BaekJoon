fc72e55157bff1341a3a74216ef49a9ad6ae36be
gibum1228 <gibum1228@naver.com>
2020-08-11 10:03:12 +0900

    단어 정렬

--- Java/src/B1181.java
+++ Java/src/B1181.java
@@ -1,61 +1,70 @@
-/*
-문제
-알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
-1. 길이가 짧은 것부터
-2. 길이가 같으면 사전 순으로
-입력
-첫째 줄에 단어의 개수 N이 주어진다. (1≤N≤20,000) 둘째 줄부터 N개의 줄에 걸쳐 알파벳 소문자로 이루어진 단어가 한 줄에 하나씩 주어진다. 
-주어지는 문자열의 길이는 50을 넘지 않는다.
-출력
-조건에 따라 정렬하여 단어들을 출력한다. 단, 같은 단어가 여러 번 입력된 경우에는 한 번씩만 출력한다.
- */
-import java.util.Scanner;
-import java.util.Arrays;
+import java.io.*;
+import java.util.ArrayList;
 
-public class B1181 {
+public class B1181 { // 각 단어 길이의 시작점을 저장하는 배열을 만들면 더 좋을 거 같음 ex) jump table
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 빠른 입출력을 위해 버퍼 사용
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int size = sc.nextInt(); // 단어의 개수
-		String[] wordNote = new String[size]; // 단어장
+		int n = Integer.parseInt(br.readLine()); // 작동 횟수
+		ArrayList<String> word = new ArrayList<String>(); // 정렬된 단어를 담을 ArrayList
 		
-		for(int i = 0; i < size; i++) { // 단어 입력받기
-			wordNote[i] = sc.next(); 
+		for(int i = 0; i < n; i++) {
+			String text = br.readLine(); // 단어 입력 받기
+			
+			if(word.isEmpty()) { // 배열이 비어 있으면 add
+				word.add(text);
+			}else { // 배열이 비어 있지 않으면 정렬하기 위해 sortWord() 호출
+				sortWord(word, text);
+			}
 		}
 		
-		Arrays.sort(wordNote); // 단어 정렬
+		for(int i = 0; i < word.size(); i++) { // 출력
+			bw.write(word.get(i) + "\n");
+		}
 		
-		String tmp; // 변경을 위한 변수
-		for(int i = 0; i < size; i++) { // 단어를 조건에 맞게 정렬하기
-			for(int j = 0; j < size; j++) {
-				if(wordNote[i].length() < wordNote[j].length()) { // 길이가 짧은 단어가 먼저 출력
-					tmp = wordNote[i];
-					wordNote[i] = wordNote[j];
-					wordNote[j] = tmp;
-				} else if(wordNote[i].length() == wordNote[j].length()) { // 단어 순서가 빠른(단어 크기가 작은) 단어가 먼저 출력
-					if(wordNote[i].compareTo(wordNote[j]) < 0) { // i가 j보다 작으면 음수(i가 j보다 단어 순서가 빠르다는 뜻)
-						tmp = wordNote[i];
-						wordNote[i] = wordNote[j];
-						wordNote[j] = tmp;
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	static public void sortWord(ArrayList<String> word, String text) {
+		for(int i = 0; i < word.size(); i++) {
+			if(word.get(i).compareTo(text) == 0) { // 중복 단어 스킵
+				break;
+			}
+			
+			if(word.get(i).length() > text.length()) { // text의 길이가 더 짧을 경우
+				
+				String tmp = word.get(i); // 교환
+				word.set(i, text);
+				word.add(i+1, tmp);
+				
+				break;
+			}else if(word.get(i).length() == text.length()){ // 길이가 같을 경우
+				
+				if(word.get(i).compareTo(text) < 0) { // text가 단어 순으로 뒤에 있을 경우
+					if(i != word.size() - 1) { // 마지막 단어와 비교하는게 아니면
+						if(word.get(i+1).length() == text.length()) { // 다음 단어와 text와 길이가 같다면 다음 단어와 비교
+							continue;
+						}
 					}
+					
+					word.add(i+1, text); // 바로 다음에 추가
+				}else { // text 단어 순이 더 앞일 경우
+					String tmp = word.get(i); // 교환
+					word.set(i, text);
+					word.add(i+1, tmp);
+				}
+				
+				break;
+			}else {
+				
+				if(i == word.size() - 1) { // 모두와 비교했을 때 text의 길이가 가장 길 때 맨 뒤에 삽입
+					word.add(i+1, text);
 				}
 			}
 		}
-		
-		// print
-		for(int i = 0; i < size - 1; i++) {
-			if(wordNote[i].compareTo(wordNote[i+1]) == 0) { // 같은 문자열이면 return값 0
-				continue;
-			}
-			System.out.println(wordNote[i]); // 단어 출력
-			if(i == size - 2) { // 마지막 단어 출력
-				System.out.print(wordNote[size-1]);
-			}
-		}
-		
-		sc.close();
 	}
-
 }
