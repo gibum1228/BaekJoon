12c620a227527ef55091922027645ec14ec17165
Kim, GiBeom <gibum1228@gmail.com>
2021-12-03 23:56:01 +0900

    수 이어 쓰기1

---
+++ 2021ing_python/implementation/B1748.py
@@ -0,0 +1,23 @@
+if __name__ == "__main__":
+    N = int(input())
+    result = 0
+    i = 1
+
+    while True:
+        num = pow(10, i)
+
+        a = N // num
+        b = N % num
+
+        if a == 0:
+            result += ((b - pow(10, i-1)) + 1) * i
+            break
+        else:
+            if i == 1:
+                result += 9
+            else:
+                result += 10 * i * 9
+
+        i += 1
+
+    print(result)
