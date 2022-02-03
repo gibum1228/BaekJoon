373a7cd5fe45842fa9003fd302e8c83d2679ab43
Kim, GiBeom <gibum1228@gmail.com>
2022-02-03 20:46:24 +0900

    안전 영역

--- 2021ing_python/graph/B2468.py
+++ 2021ing_python/graph/B2468.py
@@ -1,26 +1,41 @@
 import sys
+import copy
 
 IN = sys.stdin.readline
 
 if __name__ == "__main__":
     N = int(IN())
-    borad = [list(map(int, IN().split())) for _ in range(N)]
-    move = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    safe_zone = -1
+    board = [list(map(int, IN().split())) for _ in range(N)]
+    results = [-1]
+    original_visited = [[False for _ in range(N)] for _ in range(N)]
+    move = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
-    def bfs(i, j, h):
-        count_zone = 0
+    def bfs(i, j):
         que = [(i, j)]
+        visited[i][j] = True
 
         while que:
+            x, y = que.pop(0)
 
+            for mx, my in move:
+                dx = x + mx
+                dy = y + my
 
-        return count_zone
+                if 0 <= dx < N and 0 <= dy < N:
+                    if board[dx][dy] > n and not visited[dx][dy]:
+                        visited[dx][dy] = True
+                        que.append((dx, dy))
 
-    for h in range(1, 101):
-        visited = [[False for _ in range(N)] for _ in range(N)]
-        count_zone = 0
+    for n in range(1, max(max(board)) + 1):
+        visited = copy.deepcopy(original_visited)
+        count = 0
+
         for i in range(N):
             for j in range(N):
-                if not visited[i][j]:
-                    bfs(i, j, h)
+                if board[i][j] > n and not visited[i][j]:
+                    count += 1
+                    bfs(i, j)
+
+        results.append(count)
+
+    print(max(results))
