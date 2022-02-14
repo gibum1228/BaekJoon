066f0ad020576aa3bbba3b04b95b9f11e9e4b189
Kim, GiBeom <gibum1228@gmail.com>
2022-02-14 22:54:31 +0900

    줄 세우기

---
+++ 2021ing_python/graph/B2252.py
@@ -0,0 +1,35 @@
+import sys
+
+IN = sys.stdin.readline
+
+if __name__ == "__main__":
+    N, M = map(int, IN().split())
+    weight = [0] * (N+1)
+    graph = [[] for _ in range(N+1)]
+    visited = [False] * (N+1)
+
+    for _ in range(M):
+        A, B = map(int, IN().split())
+
+        graph[A].append(B)
+        weight[B] += 1
+
+    def bfs():
+        que = []
+        result = []
+
+        try:
+            while True:
+                index = weight.index(0)
+                visited[index] = True
+                que.append(index)
+                result.append(index)
+
+                for next in graph[index]:
+                    weight[next] -= 1
+        except:
+            pass
+
+        return result
+
+    print(" ".join(list(str, bfs())))
