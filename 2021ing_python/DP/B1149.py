b8f671ce725ac6a7379cf3ec200175c0446ad693
Kim, GiBeom <gibum1228@gmail.com>
2022-02-23 10:06:26 +0900

    RGB거리

--- 2021ing_python/DP/B1149.py
+++ 2021ing_python/DP/B1149.py
@@ -4,30 +4,14 @@ IN = sys.stdin.readline
 
 if __name__ == "__main__":
     N = int(IN())
-    dp = []
+    color = []
 
     for i in range(N):
-        dp.append([[], [], []])
-        R, G, B = map(int, IN().split())
+        color.append(list(map(int, IN().split())))
 
-        if i == 0:
-            dp[0] = [(R, "r"), (G, "g"), (B, "b")]
-        else:
-            for j in range(3):
-                if dp[i - 1][j][1] == "r":
-                    if G < B:
-                        dp[i][j] = (dp[i - 1][j][0] + G, "g")
-                    else:
-                        dp[i][j] = (dp[i - 1][j][0] + B, "b")
-                elif dp[i - 1][j][1] == "g":
-                    if R < B:
-                        dp[i][j] = (dp[i - 1][j][0] + R, "r")
-                    else:
-                        dp[i][j] = (dp[i - 1][j][0] + B, "b")
-                else:
-                    if R < G:
-                        dp[i][j] = (dp[i - 1][j][0] + R, "r")
-                    else:
-                        dp[i][j] = (dp[i - 1][j][0] + G, "g")
+    for i in range(1, N):
+        color[i][0] += min(color[i - 1][1], color[i - 1][2])
+        color[i][1] += min(color[i - 1][0], color[i - 1][2])
+        color[i][2] += min(color[i - 1][0], color[i - 1][1])
 
-    print(min(dp[-1])[0])
+    print(min(color[-1]))
