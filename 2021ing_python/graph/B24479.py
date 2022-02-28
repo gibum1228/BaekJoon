64125f5580b066fcaa83e8b12e26adcc8bf77505
Kim, GiBeom <gibum1228@gmail.com>
2022-03-01 01:47:12 +0900

    알고리즘 수업 - 깊이 우선 탐색1

--- 2021ing_python/graph/B24479.py
+++ 2021ing_python/graph/B24479.py
@@ -6,8 +6,9 @@ IN = sys.stdin.readline
 
 if __name__ == "__main__":
     N, M, R = map(int, IN().split())
-    G = [[] for _ in range(N)]
+    G = [[] for _ in range(N+1)]
     visited = [False for _ in range(N+1)]
+    results = [0 for _ in range(N+1)]
 
     for _ in range(M):
         u, v = map(int, IN().split())
@@ -15,13 +16,16 @@ if __name__ == "__main__":
         heapq.heappush(G[u], v)
         heapq.heappush(G[v], u)
 
-    def dfs(start):
+    def dfs(start, count):
+        results[start] = count
         visited[start] = True
-        print(start)
 
         for x in G[start]:
             if not visited[x]:
-                dfs(x)
+                count = dfs(x, count+1)
 
-    dfs(R)
-    print(0)
+        return count
+
+    dfs(R, 1)
+
+    print("\n".join(map(str, results[1:])))
