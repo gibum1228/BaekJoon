397113305bede256d3be8e0b2d90523e91792ca3
Kim, GiBeom <gibum1228@gmail.com>
2022-03-11 13:55:50 +0900

    알고리즘 수업 - 너비 우선 탐색 2

---
+++ 2021ing_python/graph/B24445.py
@@ -0,0 +1,39 @@
+import sys
+from unittest import result
+
+IN = sys.stdin.readline
+
+def bfs(start):
+    que = [start]
+    visited[start] = True
+    results[start] = 1
+    count = 2
+
+    while que:
+        focus_node = que.pop(0)
+
+        for next_node in G[focus_node]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                que.append(next_node)
+                results[next_node] = count
+                count += 1
+
+if __name__ == "__main__":
+    N, M, R = map(int, IN().split())
+    G = [[] for _ in range(N+1)]
+    visited = [False for _ in range(N+1)]
+    results = [0 for _ in range(N+1)]
+
+    for _ in range(M):
+        u, v = map(int, IN().split())
+
+        G[u].append(v)
+        G[v].append(u)
+    
+    for i in range(1, N+1):
+        G[i].sort(reverse=True)
+        
+    bfs(R)
+
+    print("\n".join(map(str, results[1:])))
