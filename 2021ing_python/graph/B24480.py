b5b25e16f658cce0783e27627bab9f22086f83dd
Kim, GiBeom <gibum1228@gmail.com>
2022-03-17 13:16:19 +0900

    알고리즘 수업 - 깊이 우선 탐색 2

---
+++ 2021ing_python/graph/B24480.py
@@ -0,0 +1,33 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+IN = sys.stdin.readline
+
+if __name__ == "__main__":
+    N, M, R = map(int, IN().split())
+    G = [[] for _ in range(N+1)]
+    visited = [False for _ in range(N+1)]
+    results = [0 for _ in range(N+1)]
+
+    for _ in range(M):
+        u, v = map(int, IN().split())
+
+        G[u].append(v)
+        G[v].append(u)
+    
+    for arr in G:
+        arr.sort(reverse=True)
+
+    def dfs(start, count):
+        results[start] = count
+        visited[start] = True
+
+        for x in G[start]:
+            if not visited[x]:
+                count = dfs(x, count+1)
+
+        return count
+
+    dfs(R, 1)
+
+    print("\n".join(map(str, results[1:])))
