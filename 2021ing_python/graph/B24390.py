3b1958dab9f10213db286d4b75216cdb2b668109
Kim, GiBeom <gibum1228@gmail.com>
2022-03-23 19:56:41 +0900

    또 전자레인지야?

---
+++ 2021ing_python/graph/B24390.py
@@ -0,0 +1,22 @@
+import sys
+
+IN = sys.stdin.readline
+
+if __name__ == "__main__":
+    M, S = map(int, IN().split(":"))
+    count = 1
+    
+    T = M * 60 + S
+    if T <= 30:
+        print(count)
+    else:
+        T -= 30
+        count += T // 600
+        T %= 600
+        count += T // 60
+        T %= 60
+        count += T // 30
+        T %= 30
+        count += T // 10
+        
+        print(count)
