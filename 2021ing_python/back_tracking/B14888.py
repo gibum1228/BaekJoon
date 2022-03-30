6107e190bbdd1666664be0595c3665909e03dae6
Kim, GiBeom <gibum1228@gmail.com>
2022-03-30 16:36:07 +0900

    연산자 끼워넣기

--- 2021ing_python/back_tracking/B14888.py
+++ 2021ing_python/back_tracking/B14888.py
@@ -6,50 +6,34 @@ if __name__ == "__main__":
     N = int(IN())
     num = list(map(int, IN().rstrip().split()))
     op = list(map(int, IN().rstrip().split())) # 덧셈, 뺄셈, 곱셈, 나눗셈
-    current_count = [0] * 4
     min_num, max_num = 10**9, -(10**9)
 
-    def dfs(value, current_count, start):
-        global min_num, max_num, N, num
+    def dfs(value, depth, current_count):
+        global max_num, min_num
 
-        for n in range(start, N):
-            for i in range(4):
-                if current_count[i] < op[i]:
-                    if i == 0:
-                        current_count[i] += 1
-                        n_value = value + num[n]
-                        print("=====")
-                        print(f"{n}({value})에서 연산자{i} => {n_value}")
-                        dfs(n_value, current_count, n+1)
-                        current_count[i] -= 1
-                    elif i == 1:
-                        current_count[i] += 1
-                        n_value = value - num[n]
-                        print("=====")
-                        print(f"{n}({value})에서 연산자{i} => {n_value}")
-                        dfs(n_value, current_count, n+1)
-                        current_count[i] -= 1
-                    elif i == 2:
-                        current_count[i] += 1
-                        n_value = value * num[n]
-                        print("=====")
-                        print(f"{n}({value})에서 연산자{i} => {n_value}")
-                        dfs(n_value, current_count, n+1)
-                        current_count[i] -= 1
-                    else:
-                        current_count[i] += 1
-                        n_value = -(-value // num[n]) if value < 0 else value // num[n]
-                        print("=====")
-                        print(f"{n}({value})에서 연산자{i} => {n_value}")
-                        dfs(n_value, current_count, n+1)
-                        current_count[i] -= 1
-
-        if start == N:
-            min_num = min(min_num, value)
+        if depth == len(num):
             max_num = max(max_num, value)
+            min_num = min(min_num, value)
+            return
+        
+        if current_count[0] > 0:
+            current_count[0] -= 1
+            dfs(value + num[depth], depth+1, current_count)
+            current_count[0] += 1
+        if current_count[1] > 0:
+            current_count[1] -= 1
+            dfs(value - num[depth], depth+1, current_count)
+            current_count[1] += 1
+        if current_count[2] > 0:
+            current_count[2] -= 1
+            dfs(value * num[depth], depth+1, current_count)
+            current_count[2] += 1
+        if current_count[3] > 0:
+            current_count[3] -= 1
+            dfs(value // num[depth] if value > 0 else -(-value // num[depth]), depth+1, current_count)
+            current_count[3] += 1
 
-    dfs(num[0], current_count, 1)
-
-    print("=====")
+    dfs(num[0], 1, op)
+ 
     print(max_num)
     print(min_num)
