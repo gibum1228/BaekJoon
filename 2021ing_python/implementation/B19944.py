aaf9259cf462a81b6e4094336934b1139b915dff
Kim, GiBeom <gibum1228@gmail.com>
2022-04-25 15:19:28 +0900

    뉴비의 기준은 뭘까?

---
+++ 2021ing_python/implementation/B19944.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+
+    if M <= 2:
+        print("NEWBIE!")
+    elif M <= N:
+        print("OLDBIE!")
+    else:
+        print("TLE!")
