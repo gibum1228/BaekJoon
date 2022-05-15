8a138794007b64a9f0066f262ec3f9ef80335c95
Kim, GiBeom <gibum1228@gmail.com>
2022-05-16 06:12:37 +0900

    알고리즘 수업 - 선택 정렬 1

---
+++ 2021ing_python/sorted/B23881.py
@@ -0,0 +1,25 @@
+import sys
+
+IN = sys.stdin.readline
+
+if __name__ == "__main__":
+    N, K = map(int, IN().split())
+    A = list(map(int, IN().rstrip().split()))
+    count = 0
+    results = -1
+
+    for i in range(N-1, 0, -1):
+        max_num, max_index = A[0], 0
+
+        for j in range(1, i + 1):
+            if A[j] > max_num:
+                max_num, max_index = A[j], j
+
+        if A[i] != A[max_index]:
+            A[i], A[max_index] = A[max_index], A[i]
+            count += 1
+
+        if count == K:
+            results = f"{A[max_index]} {max_num}"
+
+    print(results)
