43cbcb191a1eded26bd20119522e34d3cb67eaac
Kim, GiBeom <gibum1228@gmail.com>
2022-06-08 20:12:54 +0900

    새싹

---
+++ 2021ing_python/implementation/B25083.py
@@ -0,0 +1,13 @@
+import sys
+
+IN = sys.stdin.readline
+
+if __name__ == "__main__":
+    s = "         ,r\'\"7\n"
+    s += "r`-_   ,'  ,/\n"
+    s += " \\. \". L_r\'\n"
+    s += "   `~\\/\n"
+    s += "      |\n"
+    s += "      |"
+
+    print(s)
