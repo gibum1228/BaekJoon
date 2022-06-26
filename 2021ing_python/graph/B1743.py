e59bfa4a88f50a872561b44d47a1a2f6f19924a1
Kim, GiBeom <gibum1228@gmail.com>
2022-06-26 17:17:46 +0900

    음식물 피하기

--- 2021ing_python/graph/B1743.py
+++ 2021ing_python/graph/B1743.py
@@ -1,5 +1,40 @@
 import sys
+from collections import deque
 
 IN = sys.stdin.readline
 
 if __name__ == "__main__":
+    N, M, K = map(int, IN().split())
+    board = [[0 for _ in range(M)] for _ in range(N)]
+    visited = [[False for _ in range(M)] for _ in range(N)]
+    move = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    for _ in range(K):
+        n, m = map(int, IN().split())
+        board[n-1][m-1] = 1
+
+    def bfs(r, c):
+        que = deque([(r, c)])
+        count = 1
+
+        while que:
+            r, c = que.popleft()
+
+            for mr, mc in move:
+                dr, dc = r + mr, c + mc
+
+                if 0 <= dr < N and 0 <= dc < M and board[dr][dc] == 1 and not visited[dr][dc]:
+                    count += 1
+                    visited[dr][dc] = True
+                    que.append((dr, dc))
+
+        return count
+
+    result = 0
+    for r in range(N):
+        for c in range(M):
+            if board[r][c] == 1 and not visited[r][c]:
+                visited[r][c] = True
+                result = max(result, bfs(r, c))
+
+    print(result)
