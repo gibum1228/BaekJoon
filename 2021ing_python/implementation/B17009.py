6cabc5aedd24a63f3f1a7a505953e75656d138de
Kim, GiBeom <gibum1228@gmail.com>
2022-07-12 09:09:09 +0900

    Winning Score

---
+++ 2021ing_python/implementation/B17009.py
@@ -0,0 +1,15 @@
+import sys
+
+IN = sys.stdin.readline
+
+if __name__ == "__main__":
+    li1 = [int(input()) for _ in range(3)]
+    li2 = [int(input()) for _ in range(3)]
+    A = li1[0] * 3 + li1[1] * 2 + li1[2]
+    B = li2[0] * 3 + li2[1] * 2 + li2[2]
+    if A == B:
+        print("T")
+    elif A > B:
+        print("A")
+    else:
+        print("B")
