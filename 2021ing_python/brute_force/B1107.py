4ae3a4ab6591f78549ac4c1f1368a4b2e22940cb
Kim, GiBeom <gibum1228@gmail.com>
2022-07-30 13:52:15 +0900

    리모컨

---
+++ 2021ing_python/brute_force/B1107.py
@@ -0,0 +1,25 @@
+import sys
+
+IN = sys.stdin.readline
+
+if __name__ == "__main__":
+    target = int(IN())
+    n = int(IN())
+    broken = list(map(int, IN().rstrip().split()))
+
+    # 현재 채널에서 + 혹은 -만 사용하여 이동하는 경우
+    min_count = abs(100 - target)
+
+    for nums in range(1000001):
+        nums = str(nums)
+
+        for j in range(len(nums)):
+            # 각 숫자가 고장났는지 확인 후, 고장 났으면 break
+            if int(nums[j]) in broken:
+                break
+
+            # 고장난 숫자 없이 마지막 자리까지 왔다면 min_count 비교 후 업데이트
+            elif j == len(nums) - 1:
+                min_count = min(min_count, abs(int(nums) - target) + len(nums))
+
+    print(min_count)
