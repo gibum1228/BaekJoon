458f4f1e31b5879f21f5e3a33bbc252e70a718a9
Kim, GiBeom <gibum1228@gmail.com>
2023-01-04 23:07:25 +0900

    골뱅이 찍기 - ㅂ

--- 2021ing_python/implementation/B23808.py
+++ 2021ing_python/implementation/B23808.py
@@ -4,5 +4,12 @@ IN = sys.stdin.readline
 
 if __name__ == "__main__":
     N = int(IN())
+    arange = N*5
 
-    
+    for r in range(arange):
+        for c in range(arange):
+            if c < N or c >= arange-N or r >= arange-N or N*3-N <= r < N*3:
+                print('@', end='')
+            else:
+                print(' ', end='')
+        print()
